433120596a8bb6f12ea0294b193b7c1cc981bd82
Tobi Adeleke <tobiaadeleke@gmail.com>
2026-06-28 17:28:01 +0100

    add db connection and execute queries

--- level_1/sec_pipeline/main.py
+++ level_1/sec_pipeline/main.py
@@ -11,3 +11,16 @@ query_dict = {
     name.split('.')[0] : os.path.join(QUERY_PATH, name) 
     for name in os.listdir(QUERY_PATH) if name.endswith('.sql')
     }
+
+engine = create_engine(
+    f"postgresql://{os.environ["DB_USER"]}:{os.environ["DB_PASS"]}"
+    f"@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}"
+)
+
+with engine.connect() as conn:
+    for f_name in query_dict:
+        f_path = query_dict[f_name]
+        with open(f_path) as sql_file: 
+            query_data = sql_file.read()
+            db_result = pd.read_sql(text(query_data), con=conn)
+            query_dict[f_name] = db_result
